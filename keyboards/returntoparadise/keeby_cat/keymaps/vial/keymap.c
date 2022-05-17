88e08c3233153f04b257b0439b0f87919ec5e4b9
numanair <numanair@gmail.com>
2022-05-17 04:16:23 -0700

    keeby cat base keymap changes

--- keyboards/returntoparadise/keeby_cat/keymaps/vial/keymap.c
+++ keyboards/returntoparadise/keeby_cat/keymaps/vial/keymap.c
@@ -5,35 +5,36 @@
 #define ____ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // Base (numpad)
     [0] = LAYOUT(
         TO(1),
+        KC_P0,   KC_PDOT, KC_NUMLOCK,
         KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,
-        KC_P1,   KC_P2,   KC_P3,
-        KC_P0,   KC_PDOT, KC_ENT
+        KC_P1,   KC_P2,   KC_P3
     ),
-
+    // Browser / RES
     [1] = LAYOUT(
-        TO(0),
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS
+        TO(2),
+        KC_J,        KC_X, KC_K,
+        KC_LBRACKET, KC_X, KC_RBRACKET,
+        KC_Z,        KC_C, KC_A,
+        LCTL(S(KC_TAB)), LCTL(KC_W), LCTL(KC_TAB)
     ),
-
+    // Misc
     [2] = LAYOUT(
-        KC_TRNS,
+        TO(3),
         KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
+    // Media
     [3] = LAYOUT(
-        KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS
+        TO(0),
+        KC_MPRV, KC_MPLY, KC_MNXT,
+        KC_F13, KC_F14, KC_F15,
+        KC_F16, KC_F17, KC_F18,
+        KC_F19, KC_F20, KC_F21
     )
 };
