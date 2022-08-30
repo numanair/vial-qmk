61a02bf973540c4bc407905ca0f5d68dfd7ae041
numanair <numanair@gmail.com>
2022-08-30 13:04:47 -0700

    keeby cat: include logo and formatting fixes

--- keyboards/returntoparadise/keeby_cat/oled.c
+++ keyboards/returntoparadise/keeby_cat/oled.c
@@ -1,75 +1,23 @@
- /* Copyright 2020 Josef Adamcik
-  * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+/* Copyright 2020 Josef Adamcik
+ * Modification for VIA support and RGB underglow by Jens Bonk-Wiltfang
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-//Sets up what the OLED screens display.
+// Sets up what the OLED screens display.
 
 bool key_status_kb(void);
-
-// #ifdef OLED_DRIVER_ENABLE
-
-//     oled_write_P(qmk_logo, false);
-// }
-
-// static void print_status_narrow(void) {
-//     // Print current mode
-//     oled_write_P(PSTR("\n\n"), false);
-
-//     switch (get_highest_layer(layer_state)) {
-//         case 0:
-//             oled_write_ln_P(PSTR("Qwrt"), false);
-//             break;
-//         case 1:
-//             oled_write_ln_P(PSTR("Clmk"), false);
-//             break;
-//         default:
-//             oled_write_P(PSTR("Mod\n"), false);
-//             break;
-//     }
-//     oled_write_P(PSTR("\n\n"), false);
-//     // Print current layer
-//     oled_write_ln_P(PSTR("LAYER"), false);
-//     switch (get_highest_layer(layer_state)) {
-//         case 0:
-//         case 1:
-//             oled_write_P(PSTR("Base\n"), false);
-//             break;
-//         case 2:
-//             oled_write_P(PSTR("Raise"), false);
-//             break;
-//         case 3:
-//             oled_write_P(PSTR("Lower"), false);
-//             break;
-//         default:
-//             oled_write_ln_P(PSTR("Undef"), false);
-//     }
-//     oled_write_P(PSTR("\n\n"), false);
-//     led_t led_usb_state = host_keyboard_led_state();
-//     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
-// }
-
-// void oled_task_user(void) {
-//     if (is_keyboard_master()) {
-//         print_status_narrow();
-//     } else {
-//         render_logo();
-//     }
-// }
-
-// #endif
 
 #ifdef OLED_ENABLE
 
@@ -79,23 +27,22 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return rotation;
 }
 
-#include "oled_keeby_cat_logo.c"
+#    include "oled_keeby_cat_logo.c"
 // #include "oled_graphic_test.c"
 
-#define _BASE       0
-#define _BROWSER    1
-#define _GAME       2
-#define _MEDIA      3
+#    define _BASE 0
+#    define _BROWSER 1
+#    define _GAME 2
+#    define _MEDIA 3
 
 bool oled_task_user(void) {
-
     // Layer Status
     switch (get_highest_layer(layer_state)) {
         case _BASE:
             // Brightness
             // oled_write_P(PSTR("Base\n"), false);
             render_logo();
-            if(!is_oled_scrolling()){
+            if (!is_oled_scrolling()) {
                 oled_scroll_left();
             }
             break;
@@ -104,27 +51,38 @@ bool oled_task_user(void) {
             // oled_write_P(PSTR("Layer: "), false);
             rgb_matrix_enable_noeeprom();
             // rgb_matrix_sethsv_noeeprom(HSV_ORANGE);
-            for (uint8_t i = 0; i < 3; i++){
-                const uint8_t led_index_L1[] = {3, 4, 11};
-                rgb_matrix_set_color(led_index_L1[i],RGB_WHITE);
+            for (uint8_t i = 0; i < 3; i++) {
+                // refer to "Key Matrix to LED Index" numbers in keeby_cat.c
+                const uint8_t led_index[] = {3, 4, 11};
+                rgb_matrix_set_color(led_index[i], RGB_WHITE);
             }
-            if(is_oled_scrolling()){
+            for (uint8_t i = 0; i < 1; i++) {
+                const uint8_t led_index[] = {7};
+                rgb_matrix_set_color(led_index[i], RGB_RED);
+            }
+            rgb_matrix_set_color(1, RGB_ORANGE);
+            rgb_matrix_set_color(9, RGB_BLUE);
+
+            if (is_oled_scrolling()) {
                 oled_scroll_off();
             }
             oled_write_P(PSTR("BROWSER\n"), false);
             key_status_kb();
             break;
         case _GAME:
-            if(is_oled_scrolling()){
+            if (is_oled_scrolling()) {
                 oled_scroll_off();
             }
             oled_write_P(PSTR("GAME\n"), false);
             key_status_kb();
+
+            rgb_matrix_set_color(0,RGB_ORANGE);
+
             break;
         case _MEDIA:
             // oled_write_P(PSTR("Layer: "), false);
             oled_write_P(PSTR("MEDIA\n"), false);
-            if(is_oled_scrolling()){
+            if (is_oled_scrolling()) {
                 oled_scroll_off();
             }
             key_status_kb();
@@ -132,7 +90,7 @@ bool oled_task_user(void) {
         default:
             // Or use the write_ln shortcut over adding '\n' to the end of your string
             // oled_write_ln_P(PSTR("Undefined"), false);
-            if(is_oled_scrolling()){
+            if (is_oled_scrolling()) {
                 oled_scroll_off();
             }
             key_status_kb();
@@ -141,7 +99,7 @@ bool oled_task_user(void) {
     return false;
 }
 
-bool key_status_kb(void){
+bool key_status_kb(void) {
     // oled_write_P(PSTR("Layer: "), false);
     // Keyboard Status Indicators
     led_t led_state = host_keyboard_led_state();
