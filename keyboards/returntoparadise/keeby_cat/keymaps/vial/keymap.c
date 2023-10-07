06ab48d6f63bf1fccb81a5ec0767c8a287539bd2
numanair <numanair@gmail.com>
2023-10-07 16:11:56 -0700

    update vial layout

--- keyboards/returntoparadise/keeby_cat/keymaps/vial/keymap.c
+++ keyboards/returntoparadise/keeby_cat/keymaps/vial/keymap.c
@@ -16,34 +16,34 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base (numpad)
     [0] = LAYOUT(
-        TO(1),
-        KC_P0,   KC_PDOT, KC_NUM_LOCK,
+        KC_NUM_LOCK,
+        KC_P0,   KC_PDOT, TD(0),
         KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,
         KC_P1,   KC_P2,   KC_P3
     ),
     // Browser / RES
     [1] = LAYOUT(
-        TO(2),
-        KC_J,    KC_X, KC_K,
-        KC_LBRC, KC_X, KC_RBRC,
-        KC_Z,    KC_C, KC_A,
+        KC_TRNS,
+        KC_K,            KC_X,       KC_J,
+        KC_LBRC,         TD(1),      KC_RBRC,
+        KC_Z,            TD(2),      KC_A,
         LCTL(S(KC_TAB)), LCTL(KC_W), LCTL(KC_TAB)
     ),
     // Misc
     [2] = LAYOUT(
-        TO(3),
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS
+        KC_NO,
+        KC_ESCAPE,   KC_NO,      KC_ENTER,
+        KC_F10,      KC_F11,     KC_F12,
+        KC_DELETE,   KC_DELETE,  KC_DELETE,
+        KC_KP_MINUS, KC_KP_PLUS, RCTL(KC_F8)
     ),
     // Media
     [3] = LAYOUT(
-        TO(0),
+        KC_TRNS,
         KC_MPRV, KC_MPLY, KC_MNXT,
         KC_F13, KC_F14, KC_F15,
         KC_F16, KC_F17, KC_F18,
-        KC_F19, KC_F20, KC_F21
+        KC_F19, KC_F20, KC_TRNS
     )
 };
