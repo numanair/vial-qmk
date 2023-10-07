05c82e62fc8db521293d85879e820338dce0bdfc
numanair <numanair@gmail.com>
2023-10-06 20:18:13 -0700

    rename new key names

--- keyboards/returntoparadise/keeby_cat/keymaps/vial/keymap.c
+++ keyboards/returntoparadise/keeby_cat/keymaps/vial/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base (numpad)
     [0] = LAYOUT(
         TO(1),
-        KC_P0,   KC_PDOT, KC_NUMLOCK,
+        KC_P0,   KC_PDOT, KC_NUM_LOCK,
         KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,
         KC_P1,   KC_P2,   KC_P3
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
         TO(2),
         KC_J,        KC_X, KC_K,
-        KC_LBRACKET, KC_X, KC_RBRACKET,
+        KC_LBRC, KC_X, KC_RBRC,
         KC_Z,        KC_C, KC_A,
         LCTL(S(KC_TAB)), LCTL(KC_W), LCTL(KC_TAB)
     ),
