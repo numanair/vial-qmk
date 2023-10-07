c98150cba8fd7620862b6015cd5043cc004d8688
numanair <numanair@gmail.com>
2023-10-07 16:36:24 -0700

    Enable ENCODER_DEFAULT_POS

--- keyboards/returntoparadise/keeby_cat/keymaps/vial/config.h
+++ keyboards/returntoparadise/keeby_cat/keymaps/vial/config.h
@@ -5,3 +5,4 @@
 #define VIAL_KEYBOARD_UID {0x99, 0x8E, 0x55, 0xE9, 0x7F, 0xCF, 0xAE, 0xE8}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0,  0,  0 }
 #define VIAL_UNLOCK_COMBO_COLS { 2, 5, 8, 11 }
+#define ENCODER_DEFAULT_POS 0x3
