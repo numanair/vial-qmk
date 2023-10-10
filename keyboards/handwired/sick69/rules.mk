96747b74b5fde2c47cbdab2cad8fa1a646c40231
numanair <numanair@gmail.com>
2023-10-09 20:38:52 -0700

    SIck-69: removed old settings

--- keyboards/handwired/sick69/rules.mk
+++ keyboards/handwired/sick69/rules.mk
@@ -9,13 +9,7 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-MIDI_ENABLE = no            # Base MIDI support
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
 UNICODE_ENABLE = yes
 
 LAYOUTS = 65_ansi
